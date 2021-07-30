d7b6c165080e5f1d5ccdfbd5c794328329aa81a3
Garrett T <goodbyetonto@gmail.com>
2021-07-30 10:26:45 -0700

    .NET variable best practices
    Got rid of underscores

--- userProfileCopy.ps1
+++ userProfileCopy.ps1
@@ -1,53 +1,56 @@
 ##### Prompt for user inputs ##### 
 ### Network or Local transfer? ###
 do {
-    $Transfer_Type = Read-Host "TYPE 'local' IF YOU ARE EXECUTING SCRIPT FROM USER'S COMPUTER. TYPE 'network' IF YOU ARE EXECUTING SCRIPT REMOTELY"
+    $TransferType = Read-Host "TYPE 'local' IF YOU ARE EXECUTING SCRIPT FROM USER'S COMPUTER. TYPE 'network' IF YOU ARE EXECUTING SCRIPT REMOTELY"
     Write-Host "`n"
-} until (($Transfer_Type -match 'local' -or ($Transfer_Type -match 'network')))
-
-### Prompt for input of hostname ###
-do {
-    $Hostname = Read-Host "ENTER THE HOSTNAME OF THE COMPUTER PROFILE WILL BE COPIED FROM"
-    Write-Host "`n"
-    $Confirm = Read-Host "CONFIRM HOSTNAME"    
-    Write-Host "`n"    
-} until ($Hostname -eq $Confirm)
-
-### Retrieve user/profile name ###
-do {
-    $Get_User = Read-Host "ENTER THE USERNAME OF THE PROFILE YOU WOULD LIKE TO COPY"
-    Write-Host "`n"
-    $Confirm = Read-Host "CONFIRM USERNAME"
-    Write-Host "`n"
-} until ($Get_User -eq $Confirm)
+} until (($TransferType -match 'local' -or ($TransferType -match 'network')))
 
 ### Assign variable values for either local/network initiated script ###
-if ($Transfer_Type -eq 'network') {
+if ($TransferType -eq 'network') {
+    ### Prompt for input of hostname ###
+    do {
+        $Hostname = Read-Host "ENTER THE HOSTNAME OF THE COMPUTER PROFILE WILL BE COPIED FROM"
+        Write-Host "`n"
+        $Confirm = Read-Host "CONFIRM HOSTNAME"    
+        Write-Host "`n"    
+    } until ($Hostname -eq $Confirm)
+    ### Retrieve user/profile name ###
+    do {
+        $GetUser = Read-Host "ENTER THE USERNAME OF THE PROFILE YOU WOULD LIKE TO COPY"
+        Write-Host "`n"
+        $Confirm = Read-Host "CONFIRM USERNAME"
+        Write-Host "`n"
+    } until ($GetUser -eq $Confirm)
     ### User Profile Path for running over the network ###
-    $User_Profile = "\\$Hostname\c$\Users\$Get_User"
+    $UserProfile = "\\$Hostname\c$\Users\$GetUser"
 } 
 else {
     ### User Profile Path ###
-    $User_Profile = "C:\Users\$Get_User"
+    $Hostname = hostname
+    $GetUser = $env:username
+    $UserProfile = "C:\Users\$GetUser" 
 }
 
 ### Destination Path ###
-$Destination = "\\nasprod\helpdesk\userBups\$Hostname\$Get_User"
+$Destination = "\\nasprod\helpdesk\userBups\$Hostname\$GetUser"
 
 ### Time-Stamp ###
 $DateTime = Get-Date -Format 'yyyy-MM-dd HH-mm-ss'
 
 ### Chrome Bookmarks ###
-$Chrome_Bm = "$User_Profile\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+$ChromeBm = "$UserProfile\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
 
 ### Firefox Profile ###
-$Firefox_Prof = "$User_Profile\AppData\Roaming\Mozilla\Firefox\Profiles\*" 
+$FirefoxProf = "$UserProfile\AppData\Roaming\Mozilla\Firefox\Profiles\*" 
 
 ### Network Printer Array ###
 $PrintArray = @(Get-WMIObject Win32_Printer -ComputerName $Hostname | Where-Object { $_.Name -like "*\\*" } | Select-Object -ExpandProperty name)
 
+### Network Folder Array ###
+$NetFolders = @(Get-WMIObject Win32_MappedLogicalDisk -ComputerName $Hostname | Where-Object { $_.ProviderName -notlike "*\\csusmnt\home*" } | Select-Object -ExpandProperty ProviderName)
+
 ### Folders to exclude ###
-$Excludes_Folder = 
+$ExcludesFolder = 
 '.dotnet',
 '.vscode',
 'AppData',
@@ -58,8 +61,6 @@ $Excludes_Folder =
 'MicrosoftEdgeBackups', 
 'Nethood', 
 'OneDrive', 
-'PrintHood', 
-'Recent', 
 'SendTo', 
 'source', 
 'Start Menu', 
@@ -70,12 +71,12 @@ $Excludes_Folder =
 ### Folder Paths for Exclusion ###
 $Exclude = @()
 
-Foreach ($Folder in $Excludes_Folder) {
-    $Exclude += , "$User_Profile\$Folder"
+Foreach ($Folder in $ExcludesFolder) {
+    $Exclude += , "$UserProfile\$Folder"
 }
 
 ### Files to Exclude when using 'Robocopy' ###
-$Excludes_Files
+$ExcludesFiles
 
 ### Robocopy Staging Log Path ###
 $RoboStagingLog = "\\nasprod\helpdesk\userBups\bupLogs\staging_backup"
@@ -89,7 +90,7 @@ function Copy-WithProgress {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string] $User_Profile
+        [string] $UserProfile
         , [Parameter(Mandatory = $true)]
         [string] $Destination
         , [int] $Gap = 0 
@@ -119,14 +120,14 @@ function Copy-WithProgress {
     # TEE = Display log in stdout AND in target log file
 
     ### Assign Robocopy params to string variable ###
-    $CommonRobocopyParams = "/MIR /b /compress /XA:SH /XD $Excludes_Folder /XJD /R:5 /W:15 /MT:13 /Z /NP /NDL /NC /BYTES /NJH /NJS"
+    $CommonRobocopyParams = "/MIR /B /COMPRESS /XA:SH /XD $ExcludesFolder /XJD /R:5 /W:15 /MT:40 /Z /NP /NDL /NC /BYTES /NJH /NJS"
 
     ### Region Robocopy Staging ###
     Write-Verbose -Message 'Analyzing robocopy job ...'
     Write-Host "`n"
-    $StagingLogPath = '{0}\{1}' -f $RoboStagingLog, $Hostname + "_$Get_User" + "_$DateTime" 
+    $StagingLogPath = '{0}\{1}' -f $RoboStagingLog, $Hostname + "_$GetUser" + "_$DateTime" 
 
-    $StagingArgumentList = '"{0}" "{1}" /LOG:"{2}" /L {3}' -f $User_Profile, $Destination, $StagingLogPath, $CommonRobocopyParams
+    $StagingArgumentList = '"{0}" "{1}" /LOG:"{2}" /L {3}' -f $UserProfile, $Destination, $StagingLogPath, $CommonRobocopyParams
     Write-Verbose -Message ('Staging arguments: {0}' -f $StagingArgumentList)
     Write-Host "`n"
     Start-Process -Wait -FilePath robocopy.exe -ArgumentList $StagingArgumentList -NoNewWindow
@@ -140,8 +141,8 @@ function Copy-WithProgress {
     Write-Host "`n"
 
     ### Region Start Robocopy ###
-    $RobocopyLogPath = '{0}\{1}' -f $RoboFinalLog, $Hostname + "_$Get_User" + "_$DateTime"
-    $ArgumentList = '"{0}" "{1}" /LOG:"{2}" /ipg:{3} {4}' -f $User_Profile, $Destination, $RobocopyLogPath, $Gap, $CommonRobocopyParams
+    $RobocopyLogPath = '{0}\{1}' -f $RoboFinalLog, $Hostname + "_$GetUser" + "_$DateTime"
+    $ArgumentList = '"{0}" "{1}" /LOG:"{2}" /ipg:{3} {4}' -f $UserProfile, $Destination, $RobocopyLogPath, $Gap, $CommonRobocopyParams
     Write-Verbose -Message ('Beginning the robocopy process with arguments: {0}' -f $ArgumentList)
     $Robocopy = Start-Process -FilePath robocopy.exe -ArgumentList $ArgumentList -Verbose -PassThru -NoNewWindow
     Start-Sleep -Milliseconds 100
@@ -170,17 +171,40 @@ function Copy-WithProgress {
 }
 
 ### Start Powershell Transcript Recording ###
-Start-Transcript -Path ('{0}\{1}\{2}\{3}' -f $Transcript, $Get_User, $Hostname, $DateTime + "_copy") -NoClobber
+Start-Transcript -Path ('{0}\{1}\{2}\{3}' -f $Transcript, $GetUser, $Hostname, $DateTime + "_copy") -NoClobber
+
 ### Call Copy-WithProgress function(Robocopy) ###
-Copy-WithProgress $User_Profile $Destination -Verbose
+Copy-WithProgress $UserProfile $Destination -Verbose
+Start-Sleep -s 5
+
 ### Get Chrome Bookmark's file and copy to destination folder ###
-Copy-Item -Path $Chrome_Bm  -Destination $Destination
+Write-Host "`n"
+Write-Verbose -Message "Copying Chrome Bookmarks to $Destination" -Verbose
+Write-Host "`n"
+Copy-Item -Path $ChromeBm  -Destination $Destination
+
 ### Create Firefox Folder and copy Firefox Profile to destination folder ###
+Write-Verbose -Message "Creating folder for Firefox Profile in $Destination" -Verbose
+Write-Host "`n"
 New-Item -Path "$Destination" -Name "Firefox" -ItemType "directory"
-Copy-Item -Path "$Firefox_Prof" -Destination "$Destination\Firefox" -Recurse;
+Write-Verbose -Message "Copying Firefox Profile to $Destination\Firefox" -Verbose
+Write-Host "`n"
+Copy-Item -Path "$FirefoxProf" -Destination "$Destination\Firefox" -Recurse;
+
 ### Get Network Printers and create and save to destination folder ###
+Write-Verbose -Message "Creating network printer list to $Destination\printers" -Verbose
+Write-Host "`n"
 $PrintArray | Out-File -FilePath "$Destination\printers";
+
 ### Get list of installed apps and save to destination folder ###
-Get-AppxPackage -User "csusm\$Get_User" -PackageTypeFilter Main | Select-Object Name | Out-File -FilePath "$Destination\apps"
+Write-Verbose -Message "Creating installed application text file to $Destination\apps" -Verbose
+Write-Host "`n" 
+Get-AppxPackage -User "csusm\$GetUser" -PackageTypeFilter Main | Select-Object Name | Out-File -FilePath "$Destination\apps";
+
+### Get Network folders and create and save to destination folder ###
+Write-Verbose -Message "Creating network folders text file to $Destination\netFolders" -Verbose
+Write-Host "`n"
+$NetFolders | Out-File -FilePath "$Destination\netFolders";
+
 ### Stop Powershell Transcript Recording ###
 Stop-Transcript
